3783faefc51f14d662f53285ec3e1310e0f8ebd2
martinkztro <mcastro_20@alu.uabcs.mx>
2024-04-29 19:31:02 -0700

    style: style desktop/laptop design

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,9 +1,10 @@
 export default function SearchSection() {
     return (
-        <div className="flex flex-col items-center gap-8 mt-6">
-            <div className="flex flex-col gap-2">
-                <p className="text-slate-300 md:text-xl text-center">Ingresa tu enlace aquí abajo:</p>
-                <input type="text" placeholder="Pega tu URL aquí..." className="rounded-full px-6 w-26 md:w-[700px] py-2 bg-[#242427] border-[1px] text-slate-300 border-gray-500" /></div>
+        <div className="flex flex-col items-center gap-8 lg:gap-12 mt-6 lg:mt-8">
+            <div className="flex flex-col gap-2 lg:gap-4">
+                <p className="text-slate-300 md:text-xl lg:text-2xl text-center">Ingresa tu enlace aquí abajo:</p>
+                <input type="text" placeholder="Pega tu URL aquí..." className="rounded-full px-6 w-26 md:w-[700px] lg:w-[950px] py-2 lg:py-3 bg-[#242427] border-[1px] text-slate-300 border-gray-500" />
+            </div>
             <span>
                 <button className="bg-[#006fee] text-white py-2 px-8 md:px-12 md:text-xl mx-auto rounded-md">Convertir a MP3</button>
             </span>
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -3,7 +3,7 @@
 export default function VideoContainer() {
     return (
         <article className="flex flex-col h-80 items-center justify-center gap-5 mt-5 md:mt-24">
-            <p className="text-slate-400 text-center">Tu video debería aparecer aquí...</p>
+            <p className="text-slate-400 lg:text-lg text-center">Tu video debería aparecer aquí...</p>
         </article>
     )
 }
