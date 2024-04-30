218f3f08f02a5fbbdbc51d9c755a6c334b134f7b
martinkztro <mcastro_20@alu.uabcs.mx>
2024-04-30 14:16:13 -0700

    chore: show data into a styled  card

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import VideoContainer from "./VideoContainer";
 
 export default function SearchSection() {
     const [link, setLink] = useState('');
@@ -22,7 +23,7 @@ export default function SearchSection() {
                 }
             };
             fetch(`https://youtube-audio-video-download.p.rapidapi.com/geturl?video_url=${encodeURIComponent(link)}`, options)
-                .then(response => response.text())
+                .then(response => response.json())
                 .then(result => {
                     setData(result);
                 })
@@ -36,13 +37,23 @@ export default function SearchSection() {
     }
 
     return (
-        <div className="flex flex-col items-center gap-8 lg:gap-12 mt-6 lg:mt-8">
+        <div className="flex flex-col items-center gap-8 lg:gap-40 mt-6 lg:mt-8">
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 lg:gap-4">
                 <p className="text-slate-300 md:text-xl lg:text-2xl text-center">Ingresa tu enlace aquí abajo:</p>
-                <input onChange={handleInputChange} type="text" placeholder="Pega tu URL aquí..." className="rounded-full px-6 w-26 md:w-[700px] lg:w-[950px] py-2 lg:py-3 bg-[#242427] border-[1px] text-slate-300 border-gray-500" />
-                <button type="submit" className="bg-[#006fee] text-white py-2 px-8 md:px-12 md:text-xl mx-auto rounded-md">Convertir a MP3</button>
+                <input onChange={handleInputChange} type="text" placeholder="Pega tu URL aquí..." className="rounded-full px-6 w-[330px] md:w-[700px] lg:w-[950px] py-2 lg:py-3 bg-[#242427] border-[1px] text-slate-300 border-gray-500" />
+                <button
+                    type="submit"
+                    className={`text-white py-2 px-8 md:px-12 md:text-xl mt-5 mx-auto rounded-md ${loading ? 'bg-blue-500 opacity-50 cursor-not-allowed' : (data ? 'bg-yellow-600 font-bold' : 'bg-blue-500')}`}
+                    onClick={() => { if (data) window.location.reload(); }}
+                    disabled={loading}
+                >
+                    {loading ? "Cargando" : (data ? "Cargar otro" : "Convertir Video")}
+                </button>
             </form>
-            
+            <div className="p-3">
+                <VideoContainer data={data} loading={loading} error={error} />
+            </div>
+
         </div>
     )
 }
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,9 +1,44 @@
+import ButtonLoading from './ButtonLoading';
 
+export default function VideoContainer({ data, loading, error }) {
 
-export default function VideoContainer() {
-    return (
-        <article className="flex flex-col h-80 items-center justify-center gap-5 mt-5 md:mt-24">
-            <p className="text-slate-400 lg:text-lg text-center">Tu video debería aparecer aquí...</p>
-        </article>
-    )
+    if (loading) {
+        return <ButtonLoading />;
+    }
+
+    if (error) {
+        return <div>Algo malió sal:/</div>;
+    }
+
+    if (data) {
+        return (
+            <figure className="bg-slate-900 rounded-2xl w-[350px] md:w-[850px] h-full border-[1px] border-slate-600 flex md:flex-row flex-col gap-6 p-5">
+                <img
+                    className='aspect-video'
+                    src={data.thumbnail_url}
+                    alt="miniatura del video"
+                    style={{
+                        width: '300px',
+                        height: '200px',
+                        objectFit: 'cover'
+                    }}
+                />
+                <div className="flex flex-col gap-8 md:gap-1 justify-between">
+                    <div className="flex flex-col ">
+                        <h1 className="md:text-2xl text-xl font-black">{data.title}</h1>
+                        <h2 className="md:text-lg text-md text-slate-300 font-semibold">{data.author}</h2>
+                        <h3 className='text-light text-sm md:text-md text-slate-200'>{new Date(data.publish_date).toLocaleDateString()}</h3>
+                        <h4 className='text-light text-sm md:text-md text-slate-300'>{data.views} vistas</h4>
+                    </div>
+
+                    <div className="flex justify-center md:justify-around gap-5">
+                        <a href={data.video_high} className="bg-[#f31260] text-sm md:text-md text-center md:py-2 py-2 px-6 md:px-12 rounded-lg font-semibold">Descarga MP4</a>
+                        <a href={data.audio_high} className="bg-[#FF8B59] text-sm md:text-md text-center md:py-2 py-2 px-6 md:px-12 rounded-lg font-semibold">Descarga MP3</a>
+                    </div>
+                </div>
+            </figure>
+        );
+    }
+
+    return (<p className='text-slate-300'>Aqui deberia salir tu video cargado...</p>)
 }
